7cb4b8a448c9759ad7ae9b5614cb3294f0e1b463
avi-guha <75952780+avi-guha@users.noreply.github.com>
2025-09-24 12:52:33 -0700

    Update LineFollower.py

--- node/LineFollower.py
+++ node/LineFollower.py
@@ -6,35 +6,60 @@ from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
 
 
+## @package line_follower
+#  A ROS node for line following using image processing and PID control.
+#
+#  This node subscribes to a camera topic, processes images to detect a line,
+#  and publishes velocity commands to follow the line. If the line is lost,
+#  it attempts recovery maneuvers using the last known direction.
+
 class LineFollower:
+    ## Constructor.
+    #  Initializes the ROS node, subscribers, publishers, PID parameters,
+    #  and control/recovery settings.
     def __init__(self):
         rospy.init_node('line_follower')
 
+        ## Bridge to convert between ROS Image messages and OpenCV images.
         self.bridge = cv_bridge.CvBridge()
 
-        # Subscriber for camera and publisher for cmd_vel
+        ## Subscriber for the raw camera image.
         self.image_sub = rospy.Subscriber('/image_raw', Image, self.image_callback)
 
-        
+        ## Publisher for velocity commands.
         self.cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 
-        # PID parameters
-        self.kp = 0.05   
-        self.ki = 0.00   
-        self.kd = 0.02   
+        # --- PID parameters ---
+        ## Proportional gain.
+        self.kp = 0.05
+        ## Integral gain.
+        self.ki = 0.00
+        ## Derivative gain.
+        self.kd = 0.02
 
-        
+        ## Previous error term (for derivative calculation).
         self.prev_error = 0
+        ## Integral accumulator (for integral calculation).
         self.integral = 0
 
-        #control
-        self.forward_speed = 0.5  
+        # --- Control settings ---
+        ## Forward speed of the robot while following the line.
+        self.forward_speed = 0.5
 
-        # Recovery settings
-        self.last_direction = 0.0      
-        self.recovery_turn_speed = 3.14 
-        self.recovery_forward_speed = 0.0  
+        # --- Recovery settings ---
+        ## Last known turn direction (1.0 = left, -1.0 = right).
+        self.last_direction = 0.0
+        ## Angular speed during recovery turns.
+        self.recovery_turn_speed = 3.14
+        ## Forward speed during recovery.
+        self.recovery_forward_speed = 0.0
 
+    ## Callback function for image messages.
+    #  Converts the incoming image to grayscale, thresholds it, and attempts
+    #  to detect the line position using two regions of interest (bottom and mid).
+    #  Computes a PID-based control signal and publishes a velocity command.
+    #
+    #  @param msg The incoming ROS Image message.
     def image_callback(self, msg):
         # Convert ROS Image to OpenCV
         frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
@@ -43,6 +68,7 @@ class LineFollower:
 
         height, width = thresh.shape
 
+        # Define regions of interest (ROI)
         roi_bottom = thresh[int(0.7 * height):int(1.0 * height), int(0.2 * width):int(0.8 * width)]
         roi_mid = thresh[int(0.0 * height):int(0.5 * height), int(0.0 * width):int(1.0 * width)]
 
@@ -64,10 +90,12 @@ class LineFollower:
                 found = True
 
         if found:
+            # PID error terms
             error = cx_full - width // 2
             self.integral += error
             derivative = error - self.prev_error
 
+            # PID controller output for angular velocity
             angular_z = -(self.kp * error +
                           self.ki * self.integral +
                           self.kd * derivative)
@@ -84,13 +112,16 @@ class LineFollower:
             twist.angular.z = angular_z
 
         else:
+            # Recovery behavior when line is lost
             twist.linear.x = self.recovery_forward_speed
             twist.angular.z = self.recovery_turn_speed * self.last_direction
 
         # Publish twist command
         self.cmd_pub.publish(twist)
 
-        # Debug visualization
+        # Debug visualization (disabled by default)
+        # Uncomment for visualization in OpenCV windows
+        #
         # debug_frame = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
         # cv2.rectangle(debug_frame,
         #               (int(0.2 * width), int(0.7 * height)),
@@ -100,21 +131,19 @@ class LineFollower:
         #               (int(0.0 * width), int(0.0 * height)),
         #               (int(1.0 * width), int(0.5 * height)),
         #               (255, 0, 0), 2)
-
+        #
         # if cx_full is not None:
         #     cv2.circle(debug_frame, (cx_full, int(0.9 * height)), 5, (0, 0, 255), -1)
-
+        #
         # cv2.imshow("Line Following Dual ROI", debug_frame)
         # cv2.waitKey(1)
 
 
 if __name__ == '__main__':
+    ## Main execution block.
+    #  Instantiates the LineFollower class and starts the ROS event loop.
     try:
         LineFollower()
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
-
-
-
-
